9a90fd1b6423add69535864b17cdb7c02f5d5a0a
esperanza <esperanzatembo@booknowzambia.com>
2024-08-26 20:56:34 +0200

    button events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+
+
 const Home = () => {
+    const handleClick=() =>{
+        console.log('hello, esperanza');
+    }
+
+    const handleClickAgain = (fruit) => {
+        console.log('hello ' +  fruit);
+        
+    }
     return (
         <div className="home">
             <h1>HomePage </h1>
+            <button onClick={handleClick}> Click me</button>
+            <button onClick={() => handleClickAgain('mango')}>click me again</button>
 
         </div>
       );
